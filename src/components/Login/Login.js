fcbc71667be91bcae0106312a8d99c80fa5fcde3
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-10-20 10:42:07 +0600

    signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,36 @@
+
+
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login...</h1>
+        <div className='container'>
+            <Form className='mx-auto width p-5  mt-3  rounded border'>
+                <div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Log In
+                    </Button>
+                </div>
+                <div>
+                    <small> New in ema-john?<Link to='/signup'>SignUp</Link></small>
+                </div>
+            </Form>
         </div>
     );
 };
